b8dbaa9305b39bd3b6bb9979ae2d6fbb15b26a5f
zoonomen-APP <appeterson37@gmail.com>
2021-03-19 11:00:29 -0700

    shell scripts recovered?

--- avtax/start.sh
+++ avtax/start.sh
@@ -1,7 +1,13 @@
 alias e=egrep
-alias r=c:/zoonomen/avtax/tools/rq.sh
 alias i=c:/zoonomen/avtax/tools/i.sh
-alias q=c:/zoonomen/avtax/tools/auyr.sh
-alias b=c:/zoonomen/avtax/tools/b.sh
+alias update=c:/zoonomen/avtax/update.sh
 alias p=c:/zoonomen/avtax/tools/p.sh
+alias b=c:/zoonomen/avtax/tools/b.sh
+alias q=c:/zoonomen/avtax/tools/auyr.sh
 alias f=c:/zoonomen/avtax/tools/f.sh
+alias rq=c:/zoonomen/avtax/tools/rq.sh
+alias h=c:/zoonomen/avtax/tools/h.sh
+alias lc=c:/zoonomen/avtax/lc.sh
+alias pet=c:/zoonomen/avtax/pet.sh
+alias ioc=c:/zoonomen/avtax/ioc.sh
+alias cba=c:/zoonomen/avtax/cba.sh
--- avtax/update.sh
+++ avtax/update.sh
@@ -1,6 +1,10 @@
 #...; 2019.01.26
 # make pass.html and update numfiles
 #prevent pass.html from being copied to ./numfile
+#
+# run date script to put date and time in c:/txt/d1
+
+c:/zoonomen/avtax/tools/date.sh
 mv pass.html pass.html.tmp
 #copy any new files to ./numfile ; the -u argument copies only newer files
 # or files that do not exist in target directory.
@@ -16,6 +20,7 @@ cd c:/zoonomen/avtax/numfile
 c:/zoonomen/avtax/numfile/num.scr
 c:/zoonomen/avtax/numfile/cbmake.sh
 c:/zoonomen/avtax/numfile/zlst.scr
+cat c:/zoonomen/avtax/numfile/0*.html > ../wb.html
 
 
 cd c:/zoonomen/avtax
@@ -28,8 +33,16 @@ cat c:/zoonomen/avtax/n/bak/?.html| tr -d '\015' > c:/zoonomen/avtax/n/bak/stor/
 cp -puv c:/zoonomen/cit/jour?.html c:/zoonomen/cit/bak
 cp -puv c:/zoonomen/bio/bio?.html c:/zoonomen/bio/bak
 cp -puv c:/zoonomen/avtax/numfile/cball.html c:/zoonomen/avtax
+#2020.05.27
+#Next line creates a file that prepends file name from
+#numfile and lists output in taxonomic order.
+grep ^ /dev/null c:/zoonomen/avtax/numfile/0*.html > c:/zoonomen/avtax/cbtxfile.txt
 
+## temporarily stop znalst.sh 2020.11.13
 #./znalst.sh
 
 #$ date  +'%Y%m%d%H%M%S'
 #20191023121914
+
+#mv znls*.* c:/zoonomen/avtax/znlst/
+#mv znsponlyd.lst c:/zoonomen/avtax/znlst/
