4ecde6f0c877d0374e0d6a895e5922e24f54c6d0
zoonomen-APP <appeterson37@gmail.com>
2021-03-28 15:08:04 -0700

    updated files. Rhabdotorrhinus citation corrected.

--- avtax/cb2.sh
+++ avtax/cb2.sh
@@ -2,12 +2,15 @@
 #Need to write script to create cball.
 # then mv pass.html to pass.html.tmp then mv back after cb2 is created.
 #
-date > date.txt
+#date > date.txt
+date > c:/txt/date.txt
 mv pass.html pass.html.tmp
-cat ????.html date.txt > cball
+# 2021.03.20 add absolute ref to date
+#cat ./numfile/0*.html date.txt > c:/zoonomen/avtax/cball
+cat ./numfile/0*.html c:/txt/date.txt > c:/zoonomen/avtax/cball
 mv pass.html.tmp pass.html
 cat c:/zoonomen/avtax/cball |tr -d '\015' > c:/zoonomen/avtax/x.1
 cat c:/zoonomen/avtax/x.1 |sed -f c:/sedscr/s.sed > c:/zoonomen/avtax/x.2
 cat c:/zoonomen/avtax/x.2 |awk -f c:/awk/s1.awk > c:/zoonomen/avtax/x.3 
 cat c:/zoonomen/avtax/x.3| tr -d '\015'|sed -f c:/sedscr/tagsed > c:/zoonomen/avtax/cb2
-rm date.txt
+#rm date.txt
--- avtax/tools/update.sh
+++ avtax/tools/update.sh
@@ -24,6 +24,7 @@ cat c:/zoonomen/avtax/numfile/0*.html > ../wb.html
 
 
 cd c:/zoonomen/avtax
+# create pass.html
 cat `ls ./numfile/0*pa*.html` | tr -d '\r' > pa01.out
 awk -f c:/awk/notab.awk pa01.out > pant.out
 cat hpass.html pant.out tpass.html | tr -d '\r' > pass.html
