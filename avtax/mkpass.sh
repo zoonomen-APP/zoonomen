7ca030dc0a3006dbc6dfecfe3f0a2b56041cb4e0
zoonomen-APP <appeterson37@gmail.com>
2021-03-29 09:50:59 -0700

    An.Cient - AnalesCient. and changes in TBL project

--- avtax/mkpass.sh
+++ avtax/mkpass.sh
@@ -1,5 +1,8 @@
 # make pass.html
 #requires all passerine files in ./numfile to be up to date.
+echo make pa01.out
 cat `ls ./numfile/0*pa*.html` > pa01.out
+echo make no tab version of pa01.out as pant.out
 awk -f c:/awk/notab.awk pa01.out > pant.out
+echo cat hpass pant.out and tpass
 cat hpass.html pant.out tpass.html > pass.html
